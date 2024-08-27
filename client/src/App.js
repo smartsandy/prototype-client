aceab474a1ae4983475cff3fb8b554138f3ad8ed
smartsandy <sandysoni@gmail.com>
2024-08-26 21:57:36 -0500

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:8000/hello')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{message ? message : "Loading..."}</h1>        
+      </header>
+    </div>
+  );
+}
+
+export default App;
